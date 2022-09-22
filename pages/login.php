b41c54115995df11e6bde98ac19620fdd2203aa8
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2022-09-22 10:08:21 +0100

    Update and rename login.html to login.php

---
+++ pages/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang=&quot;en&quot;>
+    <head>
+        <meta charset=&quot;UTF-8&quot;>
+        <title>Login</title>
+        <link rel=&quot;stylesheet&quot; href=&quot;https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css&quot;>
+    </head>
+    <body>
+        <div class=&quot;container&quot;>
+            <div class=&quot;row&quot;>
+                <div class=&quot;col-md-12&quot;>
+                    <h2>Login</h2>
+                    <p>Please fill in your email and password.</p>
+                    <form action=&quot;&quot; method=&quot;post&quot;>
+                        <div class=&quot;form-group&quot;>
+                            <label>Email Address</label>
+                            <input type=&quot;email&quot; name=&quot;email&quot; class=&quot;form-control&quot; required />
+                        </div>    
+                        <div class=&quot;form-group&quot;>
+                            <label>Password</label>
+                            <input type=&quot;password&quot; name=&quot;password&quot; class=&quot;form-control&quot; required>
+                        </div>
+                        <div class=&quot;form-group&quot;>
+                            <input type=&quot;submit&quot; name=&quot;submit&quot; class=&quot;btn btn-primary&quot; value=&quot;Submit&quot;>
+                        </div>
+                        <p>Don't have an account? <a href=&quot;register.php&quot;>Register here</a>.</p>
+                    </form>
+                </div>
+            </div>
+        </div>    
+    </body>
+</html>
