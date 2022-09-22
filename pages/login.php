eee4b29aafd673301a1595727aba175453b05621
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2022-09-22 10:09:44 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,29 +1,78 @@
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+
+$error = '';
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    // validate if email is empty
+    if (empty($email)) {
+        $error .= '<p class="error">Please enter email.</p>';
+    }
+
+    // validate if password is empty
+    if (empty($password)) {
+        $error .= '<p class="error">Please enter your password.</p>';
+    }
+
+    if (empty($error)) {
+        if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+            $query->bind_param('s', $email);
+            $query->execute();
+            $row = $query->fetch();
+            if ($row) {
+                if (password_verify($password, $row['password'])) {
+                    $_SESSION["userid"] = $row['id'];
+                    $_SESSION["user"] = $row;
+
+                    // Redirect the user to welcome page
+                    header("location: welcome.php");
+                    exit;
+                } else {
+                    $error .= '<p class="error">The password is not valid.</p>';
+                }
+            } else {
+                $error .= '<p class="error">No User exist with that email address.</p>';
+            }
+        }
+        $query->close();
+    }
+    // Close connection
+    mysqli_close($db);
+}
+?>
 <!DOCTYPE html>
-<html lang=&quot;en&quot;>
+<html lang="en">
     <head>
-        <meta charset=&quot;UTF-8&quot;>
+        <meta charset="UTF-8">
         <title>Login</title>
-        <link rel=&quot;stylesheet&quot; href=&quot;https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css&quot;>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     </head>
     <body>
-        <div class=&quot;container&quot;>
-            <div class=&quot;row&quot;>
-                <div class=&quot;col-md-12&quot;>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
                     <h2>Login</h2>
                     <p>Please fill in your email and password.</p>
-                    <form action=&quot;&quot; method=&quot;post&quot;>
-                        <div class=&quot;form-group&quot;>
+                    <?php echo $error; ?>
+                    <form action="" method="post">
+                        <div class="form-group">
                             <label>Email Address</label>
-                            <input type=&quot;email&quot; name=&quot;email&quot; class=&quot;form-control&quot; required />
+                            <input type="email" name="email" class="form-control" required />
                         </div>    
-                        <div class=&quot;form-group&quot;>
+                        <div class="form-group">
                             <label>Password</label>
-                            <input type=&quot;password&quot; name=&quot;password&quot; class=&quot;form-control&quot; required>
+                            <input type="password" name="password" class="form-control" required>
                         </div>
-                        <div class=&quot;form-group&quot;>
-                            <input type=&quot;submit&quot; name=&quot;submit&quot; class=&quot;btn btn-primary&quot; value=&quot;Submit&quot;>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
                         </div>
-                        <p>Don't have an account? <a href=&quot;register.php&quot;>Register here</a>.</p>
+                        <p>Don't have an account? <a href="register.php">Register here</a>.</p>
                     </form>
                 </div>
             </div>
