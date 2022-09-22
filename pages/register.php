2e73cde19cc9f7e030e2ce9099b0fc28f028e3d7
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2022-09-22 10:05:57 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -1,37 +1,93 @@
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+
+    $fullname = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST["confirm_password"]);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+        $error = '';
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$query->bind_param('s', $email);
+	$query->execute();
+	// Store the result so we can check if the account exists in the database.
+	$query->store_result();
+        if ($query->num_rows > 0) {
+            $error .= '<p class="error">The email address is already registered!</p>';
+        } else {
+            // Validate password
+            if (strlen($password ) < 6) {
+                $error .= '<p class="error">Password must have atleast 6 characters.</p>';
+            }
+
+            // Validate confirm password
+            if (empty($confirm_password)) {
+                $error .= '<p class="error">Please enter confirm password.</p>';
+            } else {
+                if (empty($error) && ($password != $confirm_password)) {
+                    $error .= '<p class="error">Password did not match.</p>';
+                }
+            }
+            if (empty($error) ) {
+                $insertQuery = $db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?);");
+                $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
+                $result = $insertQuery->execute();
+                if ($result) {
+                    $error .= '<p class="success">Your registration was successful!</p>';
+                } else {
+                    $error .= '<p class="error">Something went wrong!</p>';
+                }
+            }
+        }
+    }
+    $query->close();
+    $insertQuery->close();
+    // Close DB connection
+    mysqli_close($db);
+}
+?>
 <!DOCTYPE html>
-<html lang=&quot;en&quot;>
+<html lang="en">
     <head>
-        <meta charset=&quot;UTF-8&quot;>
+        <meta charset="UTF-8">
         <title>Sign Up</title>
-        <link rel=&quot;stylesheet&quot; href=&quot;https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css&quot;>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     </head>
     <body>
-        <div class=&quot;container&quot;>
-            <div class=&quot;row&quot;>
-                <div class=&quot;col-md-12&quot;>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
                     <h2>Register</h2>
                     <p>Please fill this form to create an account.</p>
-                    <form action=&quot;&quot; method=&quot;post&quot;>
-                        <div class=&quot;form-group&quot;>
+                    <?php echo $success; ?>
+                    <?php echo $error; ?>
+                    <form action="" method="post">
+                        <div class="form-group">
                             <label>Full Name</label>
-                            <input type=&quot;text&quot; name=&quot;name&quot; class=&quot;form-control&quot; required>
+                            <input type="text" name="name" class="form-control" required>
                         </div>    
-                        <div class=&quot;form-group&quot;>
+                        <div class="form-group">
                             <label>Email Address</label>
-                            <input type=&quot;email&quot; name=&quot;email&quot; class=&quot;form-control&quot; required />
+                            <input type="email" name="email" class="form-control" required />
                         </div>    
-                        <div class=&quot;form-group&quot;>
+                        <div class="form-group">
                             <label>Password</label>
-                            <input type=&quot;password&quot; name=&quot;password&quot; class=&quot;form-control&quot; required>
+                            <input type="password" name="password" class="form-control" required>
                         </div>
-                        <div class=&quot;form-group&quot;>
+                        <div class="form-group">
                             <label>Confirm Password</label>
-                            <input type=&quot;password&quot; name=&quot;confirm_password&quot; class=&quot;form-control&quot; required>
+                            <input type="password" name="confirm_password" class="form-control" required>
                         </div>
-                        <div class=&quot;form-group&quot;>
-                            <input type=&quot;submit&quot; name=&quot;submit&quot; class=&quot;btn btn-primary&quot; value=&quot;Submit&quot;>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
                         </div>
-                        <p>Already have an account? <a href=&quot;login.php&quot;>Login here</a>.</p>
+                        <p>Already have an account? <a href="login.php">Login here</a>.</p>
                     </form>
                 </div>
             </div>
